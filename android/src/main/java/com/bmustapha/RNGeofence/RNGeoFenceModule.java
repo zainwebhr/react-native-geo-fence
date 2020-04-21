704baccd4cd1ab851b4cd5f6a5ba762f4707420a
zainwebhr <zain@webhumanresource.com>
2020-04-21 11:28:58 +0500

    changes

--- android/src/main/java/com/bmustapha/RNGeofence/RNGeoFenceModule.java
+++ android/src/main/java/com/bmustapha/RNGeofence/RNGeoFenceModule.java
@@ -8,10 +8,12 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.os.Looper;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
+
 import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
